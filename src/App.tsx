5e07250f805f7cf5a60f67a26aa86162734c64ee
okrohan <rohans16996@gmail.com>
2021-11-27 12:37:03 +0530

    feat: wrap up

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import {Stories} from 'stories-app'
+
+function App() {
+  return (
+    <div>
+    <h2>Instargram</h2>
+      <Stories variant={"rounded"} />
+    </div>
+  );
+}
+
+export default App;
